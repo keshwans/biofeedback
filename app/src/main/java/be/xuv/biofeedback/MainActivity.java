90a59586e518dcdf1435fa7d7b1ccac8d4b523ee
Julien Deswaef <judeswae@thoughtworks.com>
2017-04-11 15:18:49 -0400

    fullscreen

--- app/src/main/java/be/xuv/biofeedback/MainActivity.java
+++ app/src/main/java/be/xuv/biofeedback/MainActivity.java
@@ -12,6 +12,9 @@ import com.intel.heartratecore.api.HeartRateCore;
 import android.Manifest;
 import android.util.Log;
 
+import android.view.View;
+import android.view.Window;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "BioFeedback";
@@ -19,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Starts the app fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+
         setContentView(be.xuv.biofeedback.R.layout.activity_main);
         FragmentManager fragmentManager = getSupportFragmentManager();
         Sketch sketch = new Sketch();
